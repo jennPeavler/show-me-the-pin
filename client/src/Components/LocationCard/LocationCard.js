37b7e05c4787de7d6e58f0ec5c26178124495406
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-01 14:52:28 -0600

    add css for location card

--- client/src/Components/LocationCard/LocationCard.js
+++ client/src/Components/LocationCard/LocationCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const LocationCard = ({ match, nearbyPins }) => {
   return (
-    <div>
+    <div id='location-card-display'>
       {nearbyPins.map(location => {
         if(location.id === Number(match.params.id)) {
-          console.log(location);
           return (
             <div className='location-card' key={location.id}>
-              <h2>{location.name}</h2>
+              <h2 className='location-header'>{location.name}</h2>
               <div>
+                <h3>Machines:</h3>
                 {location.machine_names.map((machine ,index) => {
                   return <p key={index}>{machine}</p>
                 })}
