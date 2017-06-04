a499572614de27e8cb831dd5dbc476a3ca71d585
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-04 15:14:45 -0600

    LocationDisplay testing complete

---
+++ client/src/Components/LocationDisplay/LocationDisplay.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { LocationDisplay } from './LocationDisplay';
+import fetchMock from 'fetch-mock'
+import { mount, shallow } from 'enzyme'
+import nearbyPins from '../../mockData/nearbyPins'
+import { browserHistory } from 'react-router'
+
+describe(LocationDisplay, () => {
+  it('should render', () => {
+    const wrapper = shallow(<LocationDisplay nearbyPins={nearbyPins.locations} searched={[]} />)
+
+    expect(wrapper.length).toEqual(1)
+  })
+
+  it('should not render any locations if the user search does not find anything', () => {
+    const wrapper = shallow(<LocationDisplay
+                    nearbyPins={nearbyPins.locations}
+                    searched={['something is in here']}
+                    searchInput={true} />)
+    const locations = wrapper.find('.location-name')
+
+    expect(locations.length).toEqual(1)
+  })
+
+  it('should render all nearbyPins if the user has not searched', () => {
+    const wrapper = shallow(<LocationDisplay
+                    nearbyPins={nearbyPins.locations}
+                    searched={[]}
+                    searchInput={false}/>)
+    const locations = wrapper.find('.location-name')
+
+    expect(locations.length).toEqual(nearbyPins.locations.length)
+  })
+
+  it('should render the correct number of locations that are returned from a search', () => {
+    const wrapper = shallow(<LocationDisplay
+                    nearbyPins={nearbyPins.locations}
+                    searched={[{name: 1}, {name: 2}, {name: 3}]}
+                    searchInput={true}/>)
+    const locations = wrapper.find('.location-name')
+
+    expect(locations.length).toEqual(3)
+  })
+})
